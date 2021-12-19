756bb9fcfc183d96da4f58337dedbf805b446506
CodeOn-ArK <1930038@kiit.ac.in>
2021-12-19 15:21:20 +0530

    system calls added

--- 002Psuedo_Char_Driver/pcd.c
+++ 002Psuedo_Char_Driver/pcd.c
@@ -25,7 +25,11 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/kdev_t.h>
 
+#undef pr_fmt 
+#define pr_fmt(fmt) "%s : " fmt, __func__
 /* Psuedo Character Device's memory */
 #define DEV_MEM     512
 
@@ -35,49 +39,6 @@ char psuedo_char_device[DEV_MEM];
 dev_t device_number;    //variable to hold device number supplied by the kernel dynamically
 struct cdev pcd_cdev;   //cdev variable to hold pcd info
 
-/*
- *
-struct file_operations {
-	struct module *owner;
-	loff_t (*llseek) (struct file *, loff_t, int);
-	ssize_t (*read) (struct file *, char __user *, size_t, loff_t *);
-	ssize_t (*write) (struct file *, const char __user *, size_t, loff_t *);
-	ssize_t (*read_iter) (struct kiocb *, struct iov_iter *);
-	ssize_t (*write_iter) (struct kiocb *, struct iov_iter *);
-	int (*iterate) (struct file *, struct dir_context *);
-	int (*iterate_shared) (struct file *, struct dir_context *);
-	unsigned int (*poll) (struct file *, struct poll_table_struct *);
-	long (*unlocked_ioctl) (struct file *, unsigned int, unsigned long);
-	long (*compat_ioctl) (struct file *, unsigned int, unsigned long);
-	int (*mmap) (struct file *, struct vm_area_struct *);
-	int (*open) (struct inode *, struct file *);
-	int (*flush) (struct file *, fl_owner_t id);
-	int (*release) (struct inode *, struct file *);
-	int (*fsync) (struct file *, loff_t, loff_t, int datasync);
-	int (*fasync) (int, struct file *, int);
-	int (*lock) (struct file *, int, struct file_lock *);
-	ssize_t (*sendpage) (struct file *, struct page *, int, size_t, loff_t *, int);
-	unsigned long (*get_unmapped_area)(struct file *, unsigned long, unsigned long, unsigned long, unsigned long);
-	int (*check_flags)(int);
-	int (*setfl)(struct file *, unsigned long);
-	int (*flock) (struct file *, int, struct file_lock *);
-	ssize_t (*splice_write)(struct pipe_inode_info *, struct file *, loff_t *, size_t, unsigned int);
-	ssize_t (*splice_read)(struct file *, loff_t *, struct pipe_inode_info *, size_t, unsigned int);
-	int (*setlease)(struct file *, long, struct file_lock **, void **);
-	long (*fallocate)(struct file *file, int mode, loff_t offset,
-			  loff_t len);
-	void (*show_fdinfo)(struct seq_file *m, struct file *f);
-#ifndef CONFIG_MMU
-	unsigned (*mmap_capabilities)(struct file *);
-#endif
-	ssize_t (*copy_file_range)(struct file *, loff_t, struct file *,
-			loff_t, size_t, unsigned int);
-	int (*clone_file_range)(struct file *, loff_t, struct file *, loff_t,
-			u64);
-	ssize_t (*dedupe_file_range)(struct file *, u64, u64, struct file *,
-			u64);
-};
- */
 
 /* HEADERs Section  */
 loff_t pcd_llseek (struct file *fp, loff_t off, int whence);
@@ -90,28 +51,34 @@ int pcd_release (struct inode *pinode, struct file *fp);
 
 loff_t pcd_llseek (struct file *fp, loff_t off, int whence)
 {
+  pr_info("lseek requested\n");
   return 0;
 }
 ssize_t pcd_read (struct file *fp, char __user *user_buf, size_t count, loff_t *f_pos)
 {
+  pr_info("read requested for %zu bytes\n", count);
   return 0;
 }
 ssize_t pcd_write (struct file *fp, const char __user *buf, size_t count, loff_t *f_pos)
 {
+  pr_info("write requested for %zu bytes\n",count);
   return 0;
 }
 int pcd_open (struct inode *pinode, struct file *fp)
 {
+  pr_info("open was successful\n");
   return 0;
 }
 int pcd_release (struct inode *pinode, struct file *fp)
 {
+  pr_info("close was successful\n");
   return 0;
 }
 
 struct file_operations pcd_fops =  
 {
   //file operations variable to hold the systemcalls implemented for the module
+  .owner  = THIS_MODULE,
   .llseek = pcd_llseek,
   .read   = pcd_read,
   .write  = pcd_write,
@@ -119,11 +86,16 @@ struct file_operations pcd_fops =
   .release= pcd_release
 };
 
+struct class *class_pcd;
+struct device *device_pcd;
+
 static int __init pcd_init(void)
 {
 /* Drivers entry point  */
   //1.  Dynamically allocate a device number 
   alloc_chrdev_region(&device_number, 0, 1, "psuedo character device");
+
+  pr_info("Device number <major>:<minor> = %d:%d\n", MAJOR(device_number), MINOR(device_number));
 
   //2.  Init the cdev structure with fops
   cdev_init(&pcd_cdev, &pcd_fops);
@@ -132,13 +104,26 @@ static int __init pcd_init(void)
   pcd_cdev.owner = THIS_MODULE;
   cdev_add(&pcd_cdev, device_number, 1);
 
-  //4.
+  //4.  Create a device class under /sys/class
+  class_pcd =  class_create(THIS_MODULE, "pcd_class");
+
+  //5.  Populate the sysfs with device information
+  device_pcd = device_create(class_pcd, NULL, device_number, NULL, "pcd");
+
+  pr_info("Module Init was successful\n" );
+
   return 0;
 }
 
 static void __exit pcd_exit(void)
 {
 /* Drivers exit point  */
+  device_destroy(class_pcd, device_number);
+  class_destroy(class_pcd);
+  cdev_del(&pcd_cdev);
+  unregister_chrdev_region(device_number, 1);
+
+  pr_info("Module Unloaded\n");
 
 }
 
