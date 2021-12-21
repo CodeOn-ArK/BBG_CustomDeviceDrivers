0bbcec7db49d3e3df4705868596218ac1b40be72
CodeOn-ArK <1930038@kiit.ac.in>
2021-12-21 20:07:08 +0530

    read implemented

--- 002Psuedo_Char_Driver/pcd.c
+++ 002Psuedo_Char_Driver/pcd.c
@@ -30,14 +30,15 @@
 
 #undef pr_fmt 
 #define pr_fmt(fmt) "%s : " fmt, __func__
+
 /* Psuedo Character Device's memory */
 #define DEV_MEM     512
 
 /* psuedo character device  */
-char psuedo_char_device[DEV_MEM];
+char pcd_buf[DEV_MEM];
 
-dev_t device_number;    //variable to hold device number supplied by the kernel dynamically
-struct cdev pcd_cdev;   //cdev variable to hold pcd info
+dev_t device_number;    /* variable to hold device number supplied by the kernel dynamically*/
+struct cdev pcd_cdev;   /* cdev variable to hold pcd info */
 
 
 /* HEADERs Section  */
@@ -57,11 +58,29 @@ loff_t pcd_llseek (struct file *fp, loff_t off, int whence)
 ssize_t pcd_read (struct file *fp, char __user *user_buf, size_t count, loff_t *f_pos)
 {
   pr_info("read requested for %zu bytes\n", count);
-  return 0;
-}
-ssize_t pcd_write (struct file *fp, const char __user *buf, size_t count, loff_t *f_pos)
-{
-  pr_info("write requested for %zu bytes\n",count);
+  pr_info("current file position = %lld\n", *f_pos);
+
+  /* Adjust the count  */
+  if(*f_pos + count > DEV_MEM){
+    count = DEV_MEM - *f_pos;   
+  }                        
+
+  /* Copy to user  */
+  if (copy_to_user(buff, pcd_buf[*f_pos], count)){
+    return -EFAULT; 
+  }
+
+  /* Update the current file position  */
+  *f_pos += count;
+
+  pr_info("bytes successfuly read = %zu bytes\n", count);
+  pr_info("updated file position = %lld\n", *f_pos);
+
+  return count;                 
+}                          
+ssize_t pcd_write (struct f ile *fp, const char __user *buf, size_t count, loff_t *f_pos)
+{                           
+  pr_info("write requested  for %zu bytes\n",count);
   return 0;
 }
 int pcd_open (struct inode *pinode, struct file *fp)
@@ -77,7 +96,7 @@ int pcd_release (struct inode *pinode, struct file *fp)
 
 struct file_operations pcd_fops =  
 {
-  //file operations variable to hold the systemcalls implemented for the module
+  /* file operations variable to hold the systemcalls implemented for the module */
   .owner  = THIS_MODULE,
   .llseek = pcd_llseek,
   .read   = pcd_read,
@@ -92,22 +111,22 @@ struct device *device_pcd;
 static int __init pcd_init(void)
 {
 /* Drivers entry point  */
-  //1.  Dynamically allocate a device number 
+  /* 1.  Dynamically allocate a device number */
   alloc_chrdev_region(&device_number, 0, 1, "psuedo character device");
 
   pr_info("Device number <major>:<minor> = %d:%d\n", MAJOR(device_number), MINOR(device_number));
 
-  //2.  Init the cdev structure with fops
+  /* 2.  Init the cdev structure with the file operation variable */
   cdev_init(&pcd_cdev, &pcd_fops);
 
-  //3.  Register the pcd's cdev structure with the kernel's VFS
-  pcd_cdev.owner = THIS_MODULE;
+  /* 3.  Register the pcd's cdev structure with the kernel's VFS */
+  pcd_cdev.owner = THIS_MODULE;       /* This tells that this module is the owner of the associated cdev struct*/
   cdev_add(&pcd_cdev, device_number, 1);
 
-  //4.  Create a device class under /sys/class
+  /* 4.  Create a device class under /sys/class */
   class_pcd =  class_create(THIS_MODULE, "pcd_class");
 
-  //5.  Populate the sysfs with device information
+  /* 5.  Populate the sysfs with device information */
   device_pcd = device_create(class_pcd, NULL, device_number, NULL, "pcd");
 
   pr_info("Module Init was successful\n" );
@@ -117,7 +136,9 @@ static int __init pcd_init(void)
 
 static void __exit pcd_exit(void)
 {
-/* Drivers exit point  */
+/* Drivers exit point 
+ * To deinitialize follow the exact reverse order as for initialization
+ */
   device_destroy(class_pcd, device_number);
   class_destroy(class_pcd);
   cdev_del(&pcd_cdev);
@@ -130,13 +151,13 @@ static void __exit pcd_exit(void)
 /*
  * REGISTRATION SECTION
  */
-module_init(pcd_init);     //Used to tell the build system about the init funx
-module_exit(pcd_exit);     //Used to tell the build system about the exit funx
+module_init(pcd_init);     /* Used to tell the build system about the init funx*/
+module_exit(pcd_exit);     /* Used to tell the build system about the exit funx*/
 
 /*
  * MODULE DESCRIPTION
  */
-MODULE_LICENSE("GPL");            //This provides information about license; beware to use proprietary licenses
+MODULE_LICENSE("GPL");            /* This provides information about license; beware to use proprietary licenses*/
 MODULE_AUTHOR("ARK");
 MODULE_DESCRIPTION("A small kernel module to inteface a psuedo char device");
 MODULE_INFO(board, "BeagleBone Green v1");
