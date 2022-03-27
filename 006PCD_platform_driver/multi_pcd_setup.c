b1c42644cf3cd38833792812eea891191fd5572d
CodeOn-ArK <1930038@kiit.ac.in>
2022-03-27 23:47:54 +0530

    platform devices created

--- 006PCD_platform_driver/multi_pcd_setup.c
+++ 006PCD_platform_driver/multi_pcd_setup.c
@@ -23,18 +23,76 @@
  * Linux kernel headers are located separetly inside linux folder in the kernel source tree
  */
 #include <linux/module.h>
+#include <linux/platform_device.h>
+#include "platform.h"
+
+#define RDO       0
+#define WRO       1
+#define RDWR      2
+
+
+/* 1. Create Platform data for the devices  */
+struct platform_device_data pcdev_pdata[2] = {
+    [0]  = {
+      .perm = RDWR,
+      .str  = "Platform_PCDEV_0",
+      .size = 512
+      
+    },
+    [1]  = {
+      .perm = RDWR,
+      .str  = "Platform_PCDEV_1",
+      .size = 512
+      
+    }
+};
+
+
+/* 2. Create Platform devices  */
+struct platform_device pcdev[2] = {
+    [0] = {
+      .name = "platform_pcdev-0",
+      .id   = 0,
+      .dev = {
+        .platform_data = &pcdev_pdata[0]
+      }
+    },
+    [1] = {
+      .name = "platform_pcdev-1",
+      .id   = 1,
+      .dev = {
+        .platform_data = &pcdev_pdata[1]
+      }
+    }
+};
+
+
+static int __init platform_pcdev_init(void){
+      platform_device_register(&pcdev[0]);
+      platform_device_register(&pcdev[1]);
+      return 0;
+
+}
+
+
+static void __exit platform_pcdev_exit(void){
+      platform_device_register(&pcdev[0]);
+      platform_device_register(&pcdev[1]);
+
+}
+
 
 
 /*
  * REGISTRATION SECTION
  */
-module_init(pcd_init);     /* Used to tell the build system about the init funx*/
-module_exit(pcd_exit);     /* Used to tell the build system about the exit funx*/
+module_init(platform_pcdev_init);     /* Used to tell the build system about the init funx*/
+module_exit(platform_pcdev_exit);     /* Used to tell the build system about the exit funx*/
 
 /*
  * MODULE DESCRIPTION
  */
 MODULE_LICENSE("GPL");            /* This provides information about license; beware to use proprietary licenses*/
 MODULE_AUTHOR("ARK");
-MODULE_DESCRIPTION("A small kernel module to inteface multiple psuedo char device");
+MODULE_DESCRIPTION("A small kernel module to inteface multiple psuedo character platform devices");
 MODULE_INFO(board, "BeagleBone Green v1");
